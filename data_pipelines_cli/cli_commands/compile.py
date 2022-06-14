de1053f047bdc2edd180d2a8f3df55e5224cf178
Andrzej Dackiewicz <andrzej.dackiewicz@getindata.com>
2022-06-14 15:01:45 +0200

    DATA-279 Empty as default image tag

--- data_pipelines_cli/cli_commands/compile.py
+++ data_pipelines_cli/cli_commands/compile.py
@@ -64,10 +64,10 @@ def _copy_dbt_manifest() -> None:
     )
 
 
-def replace_image_settings(docker_args: DockerArgs) -> None:
+def replace_image_settings(image_tag: str) -> None:
     k8s_config = BUILD_DIR.joinpath("dag", "config", "base", "execution_env.yml")
-    echo_info(f"Replacing {IMAGE_TAG_TO_REPLACE} with image tag = {docker_args.image_tag}")
-    replace(k8s_config, IMAGE_TAG_TO_REPLACE, docker_args.image_tag)
+    echo_info(f"Replacing {IMAGE_TAG_TO_REPLACE} with image tag = {image_tag}")
+    replace(k8s_config, IMAGE_TAG_TO_REPLACE, image_tag)
 
 
 def _replace_datahub_with_jinja_vars(env: str) -> None:
@@ -111,8 +111,7 @@ def compile_project(
 
     docker_args = DockerArgs(env, docker_tag, docker_build_args or {})
 
-    if docker_args.image_tag:
-        replace_image_settings(docker_args)
+    replace_image_settings(docker_args.image_tag or "Empty")
 
     _replace_datahub_with_jinja_vars(env)
 
