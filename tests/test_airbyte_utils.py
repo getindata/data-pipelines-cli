1b0ecd9e040f92b75f340f72034209f0e18fa1be
Piotr Sierkin <piotr.sierkin@getindata.com>
2022-09-15 13:54:49 +0200

    chore: add test for update file

--- tests/test_airbyte_utils.py
+++ tests/test_airbyte_utils.py
@@ -1,12 +1,43 @@
-from unittest.mock import patch
-import unittest
-from data_pipelines_cli.airbyte_utils import env_replacer
+import copy
 import os
+import pathlib
+import tempfile
+import unittest
+
+import yaml
+
+from data_pipelines_cli.airbyte_utils import env_replacer, update_file
+
+
+def read_file(file_path: pathlib.Path):
+    with open(file_path, "r") as airbyte_config_file:
+        airbyte_config = yaml.safe_load(airbyte_config_file)
+    return airbyte_config
+
 
 class AirbyteUtilsTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.airbyte_file = pathlib.Path(__file__).parent.joinpath(
+            "goldens/config/airbyte/airbyte.yml"
+        )
+        self.airbyte_config = read_file(self.airbyte_file)
+        self.airbyte_url = self.airbyte_config["airbyte_url"]
+
     def test_env_replacer(self):
-        os.environ['POSTGRES_BQ_CONNECTION'] = "123"
+        os.environ["POSTGRES_BQ_CONNECTION"] = "123"
         input = {"connection_id": "${POSTGRES_BQ_CONNECTION}"}
         valid_output = {"connection_id": "123"}
         test_output = env_replacer(input)
         self.assertDictEqual(valid_output, test_output)
+
+    def test_update_file(self):
+        config = copy.deepcopy(self.airbyte_config)
+        config["tasks"][0]["connectionId"] = 123
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            with open(pathlib.Path(tmp_dir).joinpath("airbyte.yml"), "w") as airbyte_file:
+                yaml.safe_dump(config, airbyte_file)
+
+            update_file(config, pathlib.Path(tmp_dir).joinpath("airbyte.yml"))
+            with open(pathlib.Path(tmp_dir).joinpath("airbyte.yml"), "r") as airbyte_file:
+                airbyte_config = yaml.safe_load(airbyte_file)
+                self.assertDictEqual(config, airbyte_config)
