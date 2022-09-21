b8266cfc2a29b9e0a74052a871ef50f681cdef0d
pawelpinkos <pawel.pinkos@getindata.com>
2022-09-21 22:50:39 +0000

    tests fix, looker git with ssh

--- data_pipelines_cli/cli_commands/bi.py
+++ data_pipelines_cli/cli_commands/bi.py
@@ -4,36 +4,35 @@ from ..cli_utils import echo_info, echo_subinfo
 from ..cli_constants import BUILD_DIR
 from ..config_generation import read_dictionary_from_config_directory
 from ..looker_utils import generate_lookML_model, deploy_lookML_model
-from ..errors import NotSuppertedBIError
+from ..errors import NotSuppertedBIError, DataPipelinesError
 from typing import Any, Dict
 
 def read_bi_config(env: str) -> Dict[str, Any]:
-    """Read BI configuration file for project (``config/{env}/bi.yml``)
-    :param env: Name of the environment
-    :type env: str
-    :return: Dictionary with their keys
-    :rtype: BiConfig
-    """
     return read_dictionary_from_config_directory(
         BUILD_DIR.joinpath("dag"), env, "bi.yml"
     )
 
-def _bi_looker(env: str, generate_code: bool, deploy: bool = False) -> None:
+def _bi_looker(env: str, generate_code: bool, deploy: bool = False, key_path: str = None) -> None:
     if generate_code:
         generate_lookML_model(env)
 
     if deploy:
-        deploy_lookML_model(env)
+        if key_path is None:
+            raise DataPipelinesError(
+                "Error raised when pushing Looker code. No repository key provided. "
+                "Provide key using '--key-path' option"
+            )
+        deploy_lookML_model(key_path, env)
 
-def bi(env: str, generate_code: bool, deploy: bool) -> None:
+def bi(env: str, generate_code: bool, deploy: bool = False, key_path: str = None) -> None:
     bi_config = read_bi_config(env)
  
     if bi_config["target_bi"] == "looker":
-        _bi_looker(env, generate_code, deploy)
+        _bi_looker(env, generate_code, deploy, key_path)
     else:
         raise NotSuppertedBIError()
 
-@click.command(name="bi", help="Preparing, testing and deploying integrated BI solutions")
+@click.command(name="bi", help="Generate and push BI codes")
 @click.option(
     "--env",
     default="local",
@@ -54,5 +53,11 @@ def bi(env: str, generate_code: bool, deploy: bool) -> None:
     default=False,
     help="Deploy to BI",
 )
-def bi_command(env: str, generate_code: bool, deploy: bool) -> None:
-    bi(env, generate_code, deploy)
+@click.option(
+    "--key-path",
+    type=str,
+    required=False,
+    help="Path to the key with write access to repo with published BI code",
+)
+def bi_command(env: str, generate_code: bool, deploy: bool, key_path: str) -> None:
+    bi(env, generate_code, deploy, key_path)
--- data_pipelines_cli/looker_utils.py
+++ data_pipelines_cli/looker_utils.py
@@ -1,3 +1,4 @@
+import pathlib
 import glob
 import os
 import pathlib
