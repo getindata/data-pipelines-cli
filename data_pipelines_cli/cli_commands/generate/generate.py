3b2d5d8f2cf29431ab00e22ba68de0ece460114c
Piotr Tutak <piotr.tutak@getindata.com>
2023-09-05 11:49:50 +0200

    small fixes

--- data_pipelines_cli/cli_commands/generate/generate.py
+++ data_pipelines_cli/cli_commands/generate/generate.py
@@ -3,17 +3,14 @@ import click
 from .model_yaml import generate_model_yamls_command
 from .source_sql import generate_source_sqls_command
 from .source_yaml import generate_source_yamls_command
-from dbt_databricks_factory.cli import create_job_cli
+from .databricks_job import generate_databricks_job_command
 
 @click.group(name="generate", help="Generate additional dbt files")
 def generate_group() -> None:
     pass
 
 
-@click.command("databricks-job", help="Generate a Databricks job")
-def generate_databricks_job_command() -> None:
-    create_job_cli()
-
 generate_group.add_command(generate_model_yamls_command)
 generate_group.add_command(generate_source_sqls_command)
 generate_group.add_command(generate_source_yamls_command)
+generate_group.add_command(generate_databricks_job_command)
