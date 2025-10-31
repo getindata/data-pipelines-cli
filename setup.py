87ce7c7913a744a344dc7761c6f6edd350d1b351
Dominik Gniewek-Węgrzyn <dominik.gniewek@getindata.com>
2025-10-31 16:07:18 +0100

    fix: rollback manual version bump and changelog edit

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ EXTRA_REQUIRE = {
 
 setup(
     name="data_pipelines_cli",
-    version="0.31.0",
+    version="0.30.0",
     description="CLI for data platform",
     long_description=README,
     long_description_content_type="text/markdown",
